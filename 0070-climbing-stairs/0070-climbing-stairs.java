3856392223629b31dd3250c1bc1e4d6c6cda6cee
Vaibhav Nirmal <70758433+vaibhavnirmal2001@users.noreply.github.com>
2022-10-24 14:57:02 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (85.03%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+ public static int climbStairs(int n) {
+
+        int result=0;
+
+        if (n==0 || n==1) return n;
+        int step1=1,step2=1;
+
+        for (int i = 2; i <=n; i++) {
+            result=step1+step2;
+            step1=step2;
+            step2=result;
+        }
+        return result;
+    }
+   
+}
