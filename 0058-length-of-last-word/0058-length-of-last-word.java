5ee03277adda23ddc53d1736bb65f7019cdff1b4
Vaibhav Nirmal <70758433+vaibhavnirmal2001@users.noreply.github.com>
2022-10-28 22:19:31 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (98.05%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,12 +1,12 @@
 class Solution {
     public static int lengthOfLastWord(String s) {
         if (s.length()==1) return 1;
-        String res="";
+        StringBuilder res= new StringBuilder();
 
         for (int i=s.length()-1;i>=0;i--){
             if (Character.isAlphabetic(s.charAt(i))){
-                while (Character.isAlphabetic(s.charAt(i)) && i >=0){
-                    res+=s.charAt(i);
+                while (Character.isAlphabetic(s.charAt(i))){
+                    res.append(s.charAt(i));
                     i--;
                     if (i<0) break;
                 }
