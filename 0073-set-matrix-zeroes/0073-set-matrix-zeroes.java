649f08ed9755b7f685866302f8e2c148aa2113c6
Vaibhav Nirmal <70758433+vaibhavnirmal2001@users.noreply.github.com>
2022-11-07 13:39:08 +0530

    Time: 1 ms (97.39%), Space: 43.6 MB (95.30%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,21 +1,21 @@
 class Solution {
     public void setZeroes(int[][] a) {
-      int[] temp1=new int[a.length];
-        int[] temp2=new int[a[0].length];
-        for(int i=0;i<a.length;i++){
-            for(int j=0;j<a[0].length;j++){
-                if(a[i][j]==0){
-                    temp1[i]=1;
-                    temp2[j]=1;
+      int[] rowTrack = new int[a.length];
+        int[] columnTrack = new int[a[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                if (a[i][j]==0){
+                    rowTrack[i]=1;
+                    columnTrack[j]=1;
                 }
             }
         }
-        for(int i=0;i<a.length;i++) {
-            for(int j=0;j<a[0].length;j++){
-                if(temp1[i]==1||temp2[j]==1){
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                if (rowTrack[i]==1 || columnTrack[j]==1){
                     a[i][j]=0;
                 }
             }
-        }  
+        }
     }
 }
