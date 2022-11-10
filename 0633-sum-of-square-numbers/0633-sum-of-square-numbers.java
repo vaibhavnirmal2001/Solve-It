f47fd888eb9035d085431a81d9a8ef4f92cdf14a
Vaibhav Nirmal <70758433+vaibhavnirmal2001@users.noreply.github.com>
2022-11-10 12:59:10 +0530

    Time: 3 ms (97.78%), Space: 39 MB (92.11%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static boolean judgeSquareSum(int c) {
+         long left=0, right=(long)Math.sqrt(c);
+        while (left<=right){
+            long sum= left*left+right*right;
+            if (sum > c){
+                right--;
+            }else if (sum<c){
+                left++;
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
