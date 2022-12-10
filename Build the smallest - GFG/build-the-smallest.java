8ce44b3cd6b879d4a499eafcb0120c8cee7d479b
Vaibhav Nirmal <70758433+vaibhavnirmal2001@users.noreply.github.com>
2022-12-10 20:21:20 +0530

    Added solution - LeetHub

---
+++ Build the smallest - GFG/build-the-smallest.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            String str = read.readLine();
+            Solution ob = new Solution();
+
+            System.out.println(ob.buildLowestNumber(str,N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static String buildLowestNumber(String str, int N) {
+        // code here
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<str.length();i++) {
+            char c = str.charAt(i);
+            while(stack.size()>0 && stack.peek()>c && N>0) {
+                N--;
+                stack.pop();
+            }
+            stack.push(c);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while(stack.size()>0) {
+            char c = stack.pop();
+            if(N>0) {
+                N--;
+                continue;
+            }
+            sb.append(Character.toString(c));
+        }
+        
+        String ans = sb.reverse().toString();
+        for(int i=0;i<ans.length();i++) {
+            if(ans.charAt(i)!='0') return ans.substring(i);
+        }
+        return "0";
+    }
+
+}
