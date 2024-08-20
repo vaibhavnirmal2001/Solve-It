3c6bd3192fded0ea6ef7d07112c8daaae8f3eeb0
Vaibhav Nirmal <70758433+vaibhavnirmal2001@users.noreply.github.com>
2024-08-20 22:44:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (58.36%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,21 @@
+class Solution {
+     public int uniquePaths(int m, int n) {
+        if(m == 1 || n == 1)
+            return 1;
+        m--;
+        n--;
+        if(m < n) {              // Swap, so that m is the bigger number
+            m = m + n;
+            n = m - n;
+            m = m - n;
+        }
+        long res = 1;
+        int j = 1;
+        for(int i = m+1; i <= m+n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
+            res *= i;
+            res /= j;
+        }
+            
+        return (int)res;
+    }
+}
