59be7afbb5d64bc75d867e5d52d8a02fec9f951f
Vaibhav Nirmal <70758433+vaibhavnirmal2001@users.noreply.github.com>
2024-08-21 21:33:06 +0530

    Time: 5 ms (19.47%) | Memory: 42.4 MB (27.74%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,21 +1,14 @@
+import java.math.BigInteger;
+
 class Solution {
     public static String addBinary(String a, String b) {
         // int first= Integer.valueOf(a,2);
         // int second= Integer.valueOf(b,2);
 
         // return Integer.toBinaryString(first+second);
-        StringBuilder res = new StringBuilder();
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-        int carry = 0;
-        while(i >= 0 || j >= 0){
-            int sum = carry;
-            if(i >= 0) sum += a.charAt(i--) - '0';
-            if(j >= 0) sum += b.charAt(j--) - '0';
-            carry = sum > 1 ? 1 : 0;
-            res.append(sum % 2);
-        }
-        if(carry != 0) res.append(carry);
-        return res.reverse().toString();
+         BigInteger first = new BigInteger(a, 2);  // Convert binary string 'a' to a BigInteger
+    BigInteger second = new BigInteger(b, 2); // Convert binary string 'b' to a BigInteger
+
+    return first.add(second).toString(2);
     }
 }
