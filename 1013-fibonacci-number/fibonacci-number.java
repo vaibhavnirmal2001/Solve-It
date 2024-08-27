adb2f8ce83426119ce7ae2c83f3fdb1b25136fe5
Vaibhav Nirmal <70758433+vaibhavnirmal2001@users.noreply.github.com>
2024-08-28 00:48:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (11.58%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -7,10 +7,20 @@ class Solution {
         return dp[n];
     }
     public int fib(int n) {
-         int[] dp = new int[n+1];
-        for (int i = 0; i <= n; i++) {
-            dp[i]=-1;
+        //  int[] dp = new int[n+1];
+        // for (int i = 0; i <= n; i++) {
+        //     dp[i]=-1;
+        // }
+        // return getNthFibo(n,dp);
+    
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        int[] dp = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for (int i = 2; i <= n; i++) {
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return getNthFibo(n,dp);
+        return dp[n];
     }
 }
