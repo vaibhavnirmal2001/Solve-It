75c6b69569b20fb9a9143b28ba0634e58a07c893
Vaibhav Nirmal <70758433+vaibhavnirmal2001@users.noreply.github.com>
2026-01-09 10:34:50 +0530

    Time: 80 ms (8.12%) | Memory: 47.2 MB (6.29%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -14,12 +14,5 @@ class Solution {
         }
         return count;
     }
-    static{
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-    }
+
 }
