4c93ad5e7230c522498d0cce7d6032d84855f138
Vaibhav Nirmal <70758433+vaibhavnirmal2001@users.noreply.github.com>
2026-01-10 14:57:36 +0530

    Time: 4 ms (99.68%) | Memory: 47.3 MB (6.61%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,28 @@
+class MinStack {
+    private List<int[]> st;
+
+    public MinStack() {
+        st = new ArrayList<>();
+    }
+    
+    public void push(int val) {
+        int[] top = st.isEmpty() ? new int[]{val, val} : st.get(st.size() - 1);
+        int min_val = top[1];
+        if (min_val > val) {
+            min_val = val;
+        }
+        st.add(new int[]{val, min_val});        
+    }
+    
+    public void pop() {
+        st.remove(st.size() - 1);
+    }
+    
+    public int top() {
+        return st.isEmpty() ? -1 : st.get(st.size() - 1)[0];
+    }
+    
+    public int getMin() {
+        return st.isEmpty() ? -1 : st.get(st.size() - 1)[1];
+    }
+}
