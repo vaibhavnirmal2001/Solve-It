46391201e9144a602355ab0d26a8efe782df6ecf
Vaibhav Nirmal <70758433+vaibhavnirmal2001@users.noreply.github.com>
2026-04-14 23:43:05 +0530

    Time: 2 ms (91.20%) | Memory: 47.8 MB (43.63%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,8 +3,7 @@ class Solution {
         int index=0;
         for(int curr =0; curr<nums.length; curr++){
             if(nums[curr]!=0){
-                nums[index] = nums[curr];
-                index++;
+                nums[index++] = nums[curr];
             }
         }
 
